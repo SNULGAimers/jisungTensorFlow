d9166ba1cad0253fb38d7733762c41e8089397b5
jisung <jisung6723@gmail.com>
2024-02-15 14:10:31 +0900

    commit2

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import tensorflow as tf 
 
-DATA_DIR='/tmp/data'
-NUM_STEPS = 1000
-MINIBATCH_SIZE = 100
 
 
 mnist = tf.keras.datasets.mnist
@@ -11,9 +8,35 @@ mnist = tf.keras.datasets.mnist
 
 x_train, x_test = x_train/255.0, x_test/255.0
 
-model = tf.keras.model.Sequential([
+model = tf.keras.models.Sequential([
     tf.keras.layers.Flatten(input_shape = (28,28)),
     tf.keras.layers.Dense(128, activation='relu'),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
+
+model.compile(optimizer='adam', 
+              loss='sparse_categoriacal_crossentropy', metrics=['accuracy'])
+
+prediction = model(x_train[:1]).numpy()
+
+prediction = tf.nn.softmax(prediction).numpy()
+print(prediction)
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+loss = loss_fn(y_train[:1], prediction).numpy()
+print(loss)
+
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([
+    model,
+    tf.keras.layers.Softmax()
+])
+
+print(probability_model(x_test[:5]))
